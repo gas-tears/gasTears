93107df975610b27bb1914e728ff98e8607ce27b
Ki-Seok Hong <kiseok.hong98@gmail.com>
2022-01-30 18:05:35 -0500

    Change Google Analytics key

--- pages/app.tsx
+++ pages/app.tsx
@@ -32,8 +32,6 @@ const App: NextPage = () => {
     const gasHistoryOptions = useGasHistoryChart({ chainOverviewMap, price, viewCurrency })
     const chainDistributionOptions = useChainDistributionChart({ chainOverviewMap, price, viewCurrency })
 
-    //Test
-
     useEffect(() => {
         const { addresses } = router.query
         if (!addresses) return
